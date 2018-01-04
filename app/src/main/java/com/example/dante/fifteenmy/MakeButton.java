bb62fdf1b2e62f8b5ba14f6cf4b9e14d2e7dba4d
danteckj <danteckj@gmail.com>
2018-01-04 23:11:54 +0300

    1. Создан Лэйаут 2. Создан набросок класса генератора 15

---
+++ app/src/main/java/com/example/dante/fifteenmy/MakeButton.java
@@ -0,0 +1,23 @@
+package com.example.dante.fifteenmy;
+
+import android.widget.Button;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by Dante on 04.01.2018.
+ */
+
+public class MakeButton {
+
+    public MakeButton(){
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        Random  random = new Random(15);
+        for (int i : array){
+            Button button = (Button) 
+
+        }
+    }
+}
